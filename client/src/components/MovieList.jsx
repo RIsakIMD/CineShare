1d622d3f0de0bb90185c30ec0f3c040a8ab70cbe
Wyatt Helmick <wyatthelmick@gmail.com>
2023-09-18 21:16:01 -0400

    log and reg pages work with DB. All views and components created. Need DB for testing. More styling to be done

---
+++ client/src/components/MovieList.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const MovieList = (props) => {
+    const { movies, setMovies } = props;
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:8000/api/movies")
+            .then((res) => {
+                console.log(res.data);
+                setMovies(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+        <div className='allMovies'>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Release Date</th>
+                        <th>Date Watched</th>
+                        <th>Location</th>
+                        <th>Shared by</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((movie) => (
+                        <tr key={movie._id}>
+                            <td>
+                                <Link to={`/movies/${movie._id}`}>
+                                    <img 
+                                        src={movie.imageURL} 
+                                        alt={movie.title} 
+                                        style={{ width: '50px', height: '50px' }} 
+                                    />
+                                </Link>
+                            </td>
+                            <td><Link to={`/movies/${movie._id}`}>{movie.title}</Link></td>
+                            <td>{movie.releaseDate}</td>
+                            <td>{movie.watchedDate}</td>
+                            <td>{movie.location}</td>
+                            <td>{movie.user}</td>
+                            <td>
+                                <Link className="btn btn-warning" to={`/movies/edit/${movie._id}`}>Edit</Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default MovieList;
